937ac3dbade69746ae1d4203de3f9a8c92bbf5ec
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2023-12-05 21:59:37 +0700

    add data class UIStateSiswa, DetailSiswa

--- app/src/main/java/com/example/pam10c/model/EntryViewModel.kt
+++ app/src/main/java/com/example/pam10c/model/EntryViewModel.kt
@@ -1,2 +1,34 @@
 package com.example.pam10c.model
 
+import com.example.pam10c.data.Siswa
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telepon = telpon,
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telepon,
+)
+
