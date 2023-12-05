be2aae52cb5216f5ec7422c730f01d9071815ed1
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2023-12-05 22:08:10 +0700

    add fun creation extras aplikasi.siswa

--- app/src/main/java/com/example/pam10c/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam10c/model/PenyediaViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.pam10c.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.pam10c.AplikasiSiswa
+
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
