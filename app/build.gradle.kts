449edd3afe8779a4fd3ddbb54efd25b7f707efea
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2023-12-05 23:39:09 +0700

    finish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.pam10c"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pam10c"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
