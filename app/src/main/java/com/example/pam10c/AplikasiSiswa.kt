1827491d44d566b6182fd49ce238318c83e9d59e
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2023-12-05 22:06:02 +0700

    add class AplikasiSiswa

--- app/src/main/java/com/example/pam10c/AplikasiSiswa.kt
+++ app/src/main/java/com/example/pam10c/AplikasiSiswa.kt
@@ -1,4 +1,13 @@
 package com.example.pam10c
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.pam10c.repositori.ContainerApp
+import com.example.pam10c.repositori.ContainerDataApp
+
+class AplikasiSiswa: Application() {
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
