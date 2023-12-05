897e836dff6b1115bc4d91b23fe16d4ab863cf65
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2023-12-05 22:02:56 +0700

    add class home view model

--- app/src/main/java/com/example/pam10c/model/HomeViewModel.kt
+++ app/src/main/java/com/example/pam10c/model/HomeViewModel.kt
@@ -1,4 +1,29 @@
 package com.example.pam10c.model
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam10c.data.Siswa
+import com.example.pam10c.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+
+    companion object{
+        private const val TIMEOUT_MILIES = 5_000L
+    }
+    val homeuistate : StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
+        .filterNotNull()
+        .map { HomeUiState(listSiswa = it.toList()) }
+        .stateIn(scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILIES),
+            initialValue = HomeUiState()
+        )
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
+
 }
