a403e95dba013e8bbe64a4a3ea97b45fd86a4b52
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2023-12-05 22:09:04 +0700

    add object PenyediaViewModel

--- app/src/main/java/com/example/pam10c/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam10c/model/PenyediaViewModel.kt
@@ -2,8 +2,19 @@ package com.example.pam10c.model
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pam10c.AplikasiSiswa
 
-
+object PenyediaViewModel{
+    val factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
 fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
