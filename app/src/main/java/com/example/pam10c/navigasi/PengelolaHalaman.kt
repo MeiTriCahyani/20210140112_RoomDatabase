66a71b0d159adc3b1fa4f5a7f59823b53845b22c
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2023-12-05 23:13:44 +0700

    add fun hostnavigasi

--- app/src/main/java/com/example/pam10c/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam10c/navigasi/PengelolaHalaman.kt
@@ -11,10 +11,21 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.pam10c.R
+import com.example.pam10c.halaman.DestinasiEntry
+import com.example.pam10c.halaman.DestinasiHome
+import com.example.pam10c.halaman.EntrySiswaScreen
+import com.example.pam10c.halaman.HomeScreen
 
 
-
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController()){
+    HostNavigasi(navController = navController)
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -38,4 +49,19 @@ fun SiswaTopAppBar(
             }
         }
     )
+}
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route)})
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
 }
